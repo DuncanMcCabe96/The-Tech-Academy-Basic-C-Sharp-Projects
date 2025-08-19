6557f66adc453b3887f58c4d78874fa94ffc5105
Duncan McCabe <duncan96mccabe@gmail.com>
2025-08-19 17:46:31 -0600

    Add files via upload

---
+++ WelcomeScreen/WelcomeScreen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems! \nRemeber, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
+
