ad5545831e88e05c42caa66f56cccabd20195093
Duncan McCabe <duncan96mccabe@gmail.com>
2025-09-18 18:52:32 -0600

    Add files via upload

---
+++ ClassApp/ClassApp/MathOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassApp
+{
+    public class MathOperations
+    {
+        public void Division(int number)
+        {
+            int result = number / 2;
+            Console.WriteLine("Result: " + result);
+        }
+
+        public void MultiplyByTwo(int input, out int result)
+        {
+            result = input* 2;
+        }
+
+        public void Division(double number)
+        {
+            double result = number / 2;
+                Console.WriteLine("Result (double): " + result);
+        }
+}
+}
---
+++ ClassApp/ClassApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+            MathOperations mathOps = new MathOperations();
+
+            // Call Division method
+            mathOps.Division(UserNumber);
+
+            // Call MultiplyByTwo method using out parameter
+            int doubledNumber;
+            mathOps.MultiplyByTwo(UserNumber, out doubledNumber);
+            Console.WriteLine("Doubled number: " + doubledNumber);
+
+            double userDouble = 15.5; // example double number
+            mathOps.Division(userDouble); // calls the overloaded method
+
+            Helper.PrintWelcomeMessage(); // no need to create an instance
+
+        }
+    }
+}
