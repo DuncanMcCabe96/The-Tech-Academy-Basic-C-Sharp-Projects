f325188af50a5d3eca5b2eacf450a46f4841914f
Duncan McCabe <duncan96mccabe@gmail.com>
2025-09-20 12:46:20 -0600

    Add files via upload

---
+++ AbstractClassExample/AbstractClassExample/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbstractClassExample
+{
+    // Abstract class Person with properties and abstract method
+    public abstract class Person
+    {
+        // Properties for first name and last name
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // Abstract method SayName() - must be implemented in derived classes
+        public abstract void SayName();
+    }
+}
---
+++ AbstractClassExample/AbstractClassExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object
+            Employee employee = new Employee();
+
+            // Set first and last name properties
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            // Call the SayName method to display the name
+            employee.SayName();
+
+            // Keep console open until user presses Enter
+            Console.ReadLine();
+        }
+    }
+}
