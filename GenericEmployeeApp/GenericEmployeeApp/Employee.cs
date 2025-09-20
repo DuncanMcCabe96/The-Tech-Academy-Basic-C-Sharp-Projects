fb99d5d5eeceef50ff7bee384e023cb3c17d0e44
Duncan McCabe <duncan96mccabe@gmail.com>
2025-09-20 14:04:17 -0600

    Add files via upload

---
+++ GenericEmployeeApp/GenericEmployeeApp/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GenericEmployeeApp
+{
+    // Generic Employee class that works with any type "T"
+    public class Employee<T>
+    {
+        // A list of generic type "T" that stores the employee's things
+        public List<T> Things { get; set; }
+    }
+}
---
+++ GenericEmployeeApp/GenericEmployeeApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericEmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an Employee object where T = string
+            // "Things" will be a List<string>
+            Employee<string> employeeString = new Employee<string>
+            {
+                Things = new List<string> { "Laptop", "Notebook", "Pen" }
+            };
+
+            // Create an Employee object where T = int
+            // "Things" will be a List<int>
+            Employee<int> employeeInt = new Employee<int>
+            {
+                Things = new List<int> { 101, 202, 303 }
+            };
+
+            // Print out all Things from the Employee<string> object
+            Console.WriteLine("Employee<string> Things:");
+            foreach (string thing in employeeString.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            // Print out all Things from the Employee<int> object
+            Console.WriteLine("\nEmployee<int> Things:");
+            foreach (int thing in employeeInt.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            // Keep console window open until user presses Enter
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
